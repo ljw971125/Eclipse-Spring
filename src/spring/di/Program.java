9508620624cd1802b07172674ff24228400cc775
아이스바닐라라떼 <121101681+ljw971125@users.noreply.github.com>
2023-07-14 18:05:07 +0900

    스프링프레임워크1

---
+++ src/spring/di/Program.java
@@ -0,0 +1,28 @@
+package spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.ui.ExamConsole;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/* 스프링에게 지시하는 방법으로 코드를 변경
+		Exam exam = new NewlecExam();
+		ExamConsole console = new GridExamConsole();
+		
+		console.setExam(exam);
+		
+		console.print();
+		*/
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		ExamConsole console = (ExamConsole) context.getBean("console"); 
+		//ExamConsole console = context.getBean(ExamConsole.class); 
+		console.print();
+	}
+
+}
