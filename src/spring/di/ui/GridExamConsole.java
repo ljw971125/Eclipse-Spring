f06590cf9215c213bf2d08eaba92a2af4d0cddfc
아이스바닐라라떼 <121101681+ljw971125@users.noreply.github.com>
2023-07-18 23:39:40 +0900

    스프링프레임워크2

--- src/spring/di/ui/GridExamConsole.java
+++ src/spring/di/ui/GridExamConsole.java
@@ -18,7 +18,7 @@ public class GridExamConsole implements ExamConsole {
 		System.out.println("忙式式式式式式式式式成式式式式式式式式式忖");
 		System.out.println("弛  total  弛   avg   弛");
 		System.out.println("戍式式式式式式式式式托式式式式式式式式式扣");
-		 System.out.printf("弛   %3d   弛  %3.2f   弛\n",exam.total(),exam.avg());
+		 System.out.printf("弛   %3d   弛  %3.2f  弛\n",exam.total(),exam.avg());
 		System.out.println("戌式式式式式式式式式扛式式式式式式式式式戎");
 		
 
