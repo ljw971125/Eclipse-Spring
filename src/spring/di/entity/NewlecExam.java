6a872903bf8928323e3d8d3ae30e50fdd2843907
아이스바닐라라떼 <121101681+ljw971125@users.noreply.github.com>
2023-07-20 16:38:38 +0900

    스프링프레임워크3

--- src/spring/di/entity/NewlecExam.java
+++ src/spring/di/entity/NewlecExam.java
@@ -6,6 +6,23 @@ public class NewlecExam implements Exam {
 	private int eng;
 	private int math;
 	private int com;
+	
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+	
+	
+
+
 
 	public int getKor() {
 		return kor;
@@ -49,6 +66,13 @@ public class NewlecExam implements Exam {
 	public float avg() {
 		// TODO Auto-generated method stub
 		return total() / 4.0f;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
 	}
 
 }
